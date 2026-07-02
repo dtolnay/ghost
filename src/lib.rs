9dd33d177c4b356f166bd4acbe7bb8001392c03d
David Tolnay <dtolnay@gmail.com>
2026-07-02 10:00:18 -0700

    Release 0.1.22

--- src/lib.rs
+++ src/lib.rs
@@ -214,7 +214,7 @@
 //! # }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/ghost/0.1.21")]
+#![doc(html_root_url = "https://docs.rs/ghost/0.1.22")]
 #![allow(
     clippy::doc_markdown,
     // https://github.com/rust-lang/rust-clippy/issues/8538
